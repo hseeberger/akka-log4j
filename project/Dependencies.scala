dd7e32e81cd6de7a900c613530c8f36d975f78d3
Heiko Seeberger <mail@heikoseeberger.de>
2016-03-09 09:02:25 +0100

    Some build style fixes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,18 @@
 import sbt._
 
 object Version {
-  val akka       = "2.4.2"
-  val log4j      = "2.5"
-  val scala      = "2.11.8"
-  val scalaCheck = "1.13.0"
-  val scalaTest  = "2.2.6"
+  final val Akka       = "2.4.2"
+  final val Log4j      = "2.5"
+  final val Scala      = "2.11.8"
+  final val ScalaCheck = "1.12.5"
+  final val ScalaTest  = "2.2.6"
 }
 
 object Library {
-  val akkaActor   = "com.typesafe.akka"        %% "akka-actor"   % Version.akka
-  val akkaTestkit = "com.typesafe.akka"        %% "akka-testkit" % Version.akka
-  val log4jApi    = "org.apache.logging.log4j" %  "log4j-api"    % Version.log4j
-  val log4jCore   = "org.apache.logging.log4j" %  "log4j-core"   % Version.log4j
-  val scalaCheck  = "org.scalacheck"           %% "scalacheck"   % Version.scalaCheck
-  val scalaTest   = "org.scalatest"            %% "scalatest"    % Version.scalaTest
+  val akkaActor   = "com.typesafe.akka"        %% "akka-actor"   % Version.Akka
+  val akkaTestkit = "com.typesafe.akka"        %% "akka-testkit" % Version.Akka
+  val log4jApi    = "org.apache.logging.log4j" %  "log4j-api"    % Version.Log4j
+  val log4jCore   = "org.apache.logging.log4j" %  "log4j-core"   % Version.Log4j
+  val scalaCheck  = "org.scalacheck"           %% "scalacheck"   % Version.ScalaCheck
+  val scalaTest   = "org.scalatest"            %% "scalatest"    % Version.ScalaTest
 }
