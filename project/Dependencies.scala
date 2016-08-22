84f97a0509a551c42c3ed47ea9e4a608018a7547
Heiko Seeberger <mail@heikoseeberger.de>
2016-08-22 17:11:46 +0200

    Upgrade to Akka 2.4.9, some other upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Version {
-  final val Akka       = "2.4.7"
-  final val Log4j      = "2.6"
+  final val Akka       = "2.4.9"
+  final val Log4j      = "2.6.2"
   final val Scala      = "2.11.8"
   final val ScalaCheck = "1.13.1"
-  final val ScalaTest  = "3.0.0-RC1"
+  final val ScalaTest  = "3.0.0"
 }
 
 object Library {
